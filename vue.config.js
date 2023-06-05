c75cef85153d332835e0391ca89b77b6e9105063
Kennnnn <386973670@qq.com>
2023-06-06 00:23:20 +0800

    feat(custom): 创建了登录页面，使用的是静态的登录方式

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,33 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+// webpack.config.js
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
+module.exports = {
+  configureWebpack: config => {
+    config.plugins.push(AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }))
+    config.plugins.push(Components({
+      resolvers: [ElementPlusResolver()]
+    }))
+  },
+  devServer: {
+    https: false,
+    hotOnly: false,
+    proxy: {
+      '/api': {
+        target: 'https://lianghj.top:8888/api/private/v1/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
+}
+
+
